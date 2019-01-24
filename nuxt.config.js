e020b6fa2c90af5d058ef2d0f8b6bb91a3510176
Đào Đăng Sơn <sondd.hn@havaz.vn>
2019-01-24 09:17:01 +0700

    OK

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Havaz.vn',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Havaz.vn' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -47,7 +47,7 @@ module.exports = {
   ],
 
   axios: {
-    baseURL: 'http://localhost:9000',
+    baseURL: 'https://api.havaz.vn/',
   },
 
   /*
